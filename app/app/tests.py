99c860945dba62a50121845be5b3fa2569bafc21
Marinela Buda <marinela.buda@bayoo.net>
2025-11-30 13:00:34 +0100

    Configured database in Django.

--- app/app/tests.py
+++ app/app/tests.py
@@ -15,19 +15,3 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 7)
 
         self.assertEqual(res, 12)
-
-
-class AnotherCalcTests(SimpleTestCase):
-    """More tests for the calc module."""
-
-    def test_add_negative_numbers(self):
-        """Test adding two negative numbers together."""
-        res = calc.add(-3, -6)
-
-        self.assertEqual(res, -9)
-
-    def test_add_random_numbers(self):
-        """Test adding two random numbers together."""
-        res = calc.add(10, 16)
-
-        self.assertEqual(res, 26)
