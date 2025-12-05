7970e1d169b3157db6e5ceae678dfd9d7d5de29c
Marinela Buda <marinela.buda@bayoo.net>
2025-12-05 20:50:41 +0100

    Implemented create user API.

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,5 @@
 """app URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -18,7 +19,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +28,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 ]
